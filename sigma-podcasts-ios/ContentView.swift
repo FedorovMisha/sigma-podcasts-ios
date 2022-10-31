cf740ff9f54ea554b4ca5c261c78807c60a55f43
Fedorov Misha <m.fedorov20021@gmail.com>
2022-10-31 14:25:07 +0300

    Initial commit

---
+++ sigma-podcasts-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sigma-podcasts-ios
+//
+//  Created by Misha Fedorov on 31.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sigma-podcasts-ios/sigma_podcasts_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  sigma_podcasts_iosApp.swift
+//  sigma-podcasts-ios
+//
+//  Created by Misha Fedorov on 31.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct sigma_podcasts_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
